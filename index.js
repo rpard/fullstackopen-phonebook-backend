f9bb5d2129937f8ade4bce8a89f52a57325f8fa4
Raimundo Pardo <153950844+rpard@users.noreply.github.com>
2025-06-28 19:04:24 -0400

    add dist

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
